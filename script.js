11ac611f609f25a7fe601b63a03e7ed9e90efdea
Hennessey Olivera <holivera@cristoreyny.org>
2017-02-03 14:53:57 +0000

    setColor

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //Write your function calls below this line
+setColor(0,1,"yellow");
+setColor(0,2,"yellow");
+setColor(1,1,"yellow");
+setColor(1,2,"yellow");
+setColor(0,0,"#FFD700");
+setColor(1,0,"#FFD700");
+
+
